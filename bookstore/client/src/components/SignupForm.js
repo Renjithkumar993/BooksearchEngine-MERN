0aa4df5afd641d03281641775906b539a17f9856
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-06-10 17:49:10 -0300

    added jwt

--- bookstore/client/src/components/SignupForm.js
+++ bookstore/client/src/components/SignupForm.js
@@ -1,152 +1,104 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  const [validated, setValidated] = useState(false);
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-  const [passwordStrength, setPasswordStrength] = useState('');
+  const [addUser,{error,data}] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-
-    // Check password strength
-    const password = event.target.value;
-    let strength = '';
-
-    if (password.length > 0) {
-      if (/[a-z]/.test(password)) {
-        strength += 'Lowercase ';
-      }
-      if (/[A-Z]/.test(password)) {
-        strength += 'Uppercase ';
-      }
-      if (/[0-9]/.test(password)) {
-        strength += 'Number ';
-      }
-      if (/[!@#$%^&*]/.test(password)) {
-        strength += 'Special Character ';
-      }
-    }
-
-    setPasswordStrength(strength.trim());
+    setUserFormData({ ...userFormData ,[name]: value});
   };
+
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
+  
     const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.stopPropagation();
-    }
-
-    setValidated(true);
-
+  
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({
+        variables: {
+ ...userFormData,
+        },
+      });
+      
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
-
-    setUserFormData({ username: '', email: '', password: '' });
+  
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
   };
+  
+  
 
   return (
     <>
+      {/* This is needed for the validation functionality above */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your signup!
         </Alert>
 
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='username'>Username</Form.Label>
-          <div className='input-group'>
-            <div className='input-group-prepend'>
-              <span className='input-group-text'>
-                <FontAwesomeIcon icon={faUser} />
-              </span>
-            </div>
-            <Form.Control
-              type='text'
-              placeholder='Your username'
-              name='username'
-              onChange={handleInputChange}
-              value={userFormData.username}
-              required
-            />
-            <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
-          </div>
+          <Form.Control
+            type='text'
+            placeholder='Your username'
+            name='username'
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='email'>Email</Form.Label>
-          <div className='input-group'>
-            <div className='input-group-prepend'>
-              <span className='input-group-text'>
-                <FontAwesomeIcon icon={faUser} />
-              </span>
-            </div>
-            <Form.Control
-              type='email'
-              placeholder='Your email address'
-              name='email'
-              onChange={handleInputChange}
-              value={userFormData.email}
-              required
-            />
-            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-          </div>
+          <Form.Control
+            type='email'
+            placeholder='Your email address'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='password'>Password</Form.Label>
-          <div className='input-group'>
-            <div className='input-group-prepend'>
-              <span className='input-group-text'>
-                <FontAwesomeIcon icon={faLock} />
-              </span>
-            </div>
-            <Form.Control
-              type='password'
-              placeholder='Your password'
-              name='password'
-              onChange={handleInputChange}
-              value={userFormData.password}
-              required
-              pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)'
-              minLength={8}
-            />
-            <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-          </div>
-          {passwordStrength && (
-            <div className='password-strength'>
-              Password Strength: {passwordStrength}
-            </div>
-          )}
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
-
         <Button
           disabled={!(userFormData.username && userFormData.email && userFormData.password)}
           type='submit'
-          variant='success'
-          className='btn-block mt-4'
-        >
-          Sign Up
+          variant='success'>
+          Submit
         </Button>
       </Form>
     </>
--- bookstore/client/src/pages/SavedBooks.js
+++ bookstore/client/src/pages/SavedBooks.js
@@ -6,70 +6,51 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
+import { useQuery, useMutation } from "@apollo/client";
 import { removeBookId } from '../utils/localStorage';
 
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+
+
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+   // const [userData, setUserData] = useState({});
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+   const { loading, data } = useQuery(GET_ME);
+   console.log(data);
+   const userData = data?.me || {};
+   console.log(userData, "userData");
+   // window.location.reload();
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+   const [removeBook] = useMutation(REMOVE_BOOK, {
+       refetchQueries: [{ query: GET_ME }],
+   });
 
-        if (!token) {
-          return false;
-        }
+   // create function that accepts the book's mongo _id value as param and deletes the book from the database
+   const handleDeleteBook = async (bookId) => {
+       const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        const response = await getMe(token);
+       if (!token) {
+           return false;
+       }
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
+       try {
+           await removeBook({
+               variables: { bookId },
+           });
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+           // upon success, remove book's id from localStorage
+           removeBookId(bookId);
+       } catch (err) {
+           console.error(err);
+       }
+   };
 
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+   // if data isn't here yet, say so
+   if (loading) {
+       return <h2>LOADING...</h2>;
+   }
 
   return (
     <>
